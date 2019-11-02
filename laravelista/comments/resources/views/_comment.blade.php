b7c70583ba2607a1729ff75d9bb10fee2a396962
Hérick Wilke <herickcwilke@gmail.com>
2019-11-01 23:28:24 -0300

    Envio ao git

---
+++ laravelista/comments/resources/views/_comment.blade.php
@@ -0,0 +1,105 @@
+@inject('markdown', 'Parsedown')
+@php($markdown->setSafeMode(true))
+
+@if(isset($reply) && $reply === true)
+  <div id="comment-{{ $comment->id }}" class="media">
+@else
+  <li id="comment-{{ $comment->id }}" class="media">
+@endif
+    <img class="mr-3" style="width: 5%" src="\doctor.svg">
+    <div class="media-body">
+        <h5 class="mt-0 mb-1">{{ $comment->commenter->name ?? $comment->guest_name }} <small class="text-muted">- {{ $comment->created_at->diffForHumans() }}</small></h5>
+        <div style="white-space: pre-wrap;">{!! $markdown->line($comment->comment) !!}</div>
+
+        <div>
+            @can('reply-to-comment', $comment)
+                <button data-toggle="modal" data-target="#reply-modal-{{ $comment->id }}" class="btn btn-sm btn-link text-uppercase">Responder</button>
+            @endcan
+            @can('edit-comment', $comment)
+                <button data-toggle="modal" data-target="#comment-modal-{{ $comment->id }}" class="btn btn-sm btn-link text-uppercase">Editar</button>
+            @endcan
+            @can('delete-comment', $comment)
+                <a href="{{ url('comments/' . $comment->id) }}" onclick="event.preventDefault();document.getElementById('comment-delete-form-{{ $comment->id }}').submit();" class="btn btn-sm btn-link text-danger text-uppercase">Excluir</a>
+                <form id="comment-delete-form-{{ $comment->id }}" action="{{ url('comments/' . $comment->id) }}" method="POST" style="display: none;">
+                    @method('DELETE')
+                    @csrf
+                </form>
+            @endcan
+        </div>
+
+        @can('edit-comment', $comment)
+            <div class="modal fade" id="comment-modal-{{ $comment->id }}" tabindex="-1" role="dialog">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <form method="POST" action="{{ url('comments/' . $comment->id) }}">
+                            @method('PUT')
+                            @csrf
+                            <div class="modal-header">
+                                <h5 class="modal-title">Editar Comentário</h5>
+                                <button type="button" class="close" data-dismiss="modal">
+                                <span>&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <label for="message">Altere a sua mensagem aqui:</label>
+                                    <textarea required class="form-control" name="message" rows="3">{{ $comment->comment }}</textarea>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-sm btn-outline-secondary text-uppercase" data-dismiss="modal">Cancelar</button>
+                                <button type="submit" class="btn btn-sm btn-outline-success text-uppercase">Atualizar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endcan
+
+        @can('reply-to-comment', $comment)
+            <div class="modal fade" id="reply-modal-{{ $comment->id }}" tabindex="-1" role="dialog">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <form method="POST" action="{{ url('comments/' . $comment->id) }}">
+                            @csrf
+                            <div class="modal-header">
+                                <h5 class="modal-title">Responder comentário</h5>
+                                <button type="button" class="close" data-dismiss="modal">
+                                <span>&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <label for="message">Insira sua mensagem aqui:</label>
+                                    <textarea required class="form-control" name="message" rows="3"></textarea>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-sm btn-outline-secondary text-uppercase" data-dismiss="modal">Cancelar</button>
+                                <button type="submit" class="btn btn-sm btn-outline-success text-uppercase">Responder</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endcan
+
+        <br />{{-- Margin bottom --}}
+
+        {{-- Recursion for children --}}
+        @if($grouped_comments->has($comment->id))
+            @foreach($grouped_comments[$comment->id] as $child)
+                @include('comments::_comment', [
+                    'comment' => $child,
+                    'reply' => true,
+                    'grouped_comments' => $grouped_comments
+                ])
+            @endforeach
+        @endif
+
+    </div>
+@if(isset($reply) && $reply === true)
+  </div>
+@else
+  </li>
+@endif
